803bc6db2f58514a9946fe0979dadc78a64a300a
dewni_it18046212 <it18046212@my.sliit.lk>
2021-07-01 23:15:33 +0530

    Revert "similarity_algorithm using wordnet library"
    This reverts commit b216ae5248e5ec9a6d77cfd4291c04e3e49d76c6.

--- Verbal_Memory_BE/models/use_bert.py
+++ Verbal_Memory_BE/models/use_bert.py
@@ -1,17 +1,13 @@
 import logging
 import pyrebase
-import tensorflow as tf
-import tensorflow_hub as hub
-import nltk
-nltk.download('wordnet')
-from nltk.corpus import wordnet
+from sentence_transformers import SentenceTransformer
+
 from utils.basic import *
 from utils.ts_ss import triangle_sector_similarity
+from utils.pairwise import bert_pairwise_cos_sim
 
-module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
 
-
-class USECalculator:
+class BERTCalculator:
     def __init__(self, config, sentences):
         self.sentences = sentences
         self.method = config.method
@@ -25,17 +21,30 @@ class USECalculator:
             "angular": angular_distance,
             "inner": inner_product,
             "ts-ss": triangle_sector_similarity,
+            "pairwise": bert_pairwise_cos_sim,
+            "pairwise-idf": bert_pairwise_cos_sim,
         }
 
         if self.method not in methods:
             logging.error(f"The method you chosen is not supported yet.")
             return False
 
-        model = hub.load(module_url)
+        if "pairwise" in self.method:
+            # Use-case of Pairwise cosine similarity with IDF
+            if "idf" in self.method:
+                similarity = bert_pairwise_cos_sim(self.sentences, idf=True)
+                plot_similarity(self.sentences, similarity, self.method)
+                return
+
+            # Use-case of Pairwise cosine similarity
+            similarity = bert_pairwise_cos_sim(self.sentences)
+            plot_similarity(self.sentences, similarity, self.method)
+            return
+
+        model = SentenceTransformer("bert-base-nli-mean-tokens")
+
         if self.verbose:
             logging.info(f"Now embedding sentence...")
-        
-        
 
         config = {
             "apiKey": "AIzaSyBdXTCnelDAEe7lSl8UwguuDD0efU8ezxY",
@@ -54,39 +63,16 @@ class USECalculator:
             result = db.child("Test").child("Result").child("result").get()
             test = db.child("Test").child("Result").child("test").get()
             if(level.val()!="y" and test.val()!="z"):
-                inputStringMain = self.sentences[int(level.val())-1]
-                inputStringSecond = test.val()
-
-                inputStringMain = inputStringMain.replace('.', '')
-                inputStringSecond = inputStringSecond.replace('.', '')
-
-                listStringMain = inputStringMain.split()
-                listStringSecond = inputStringSecond.split()
-                for x in listStringMain:
-                    synonyms = []
-                    for syn in wordnet.synsets(x):
-                        for l in syn.lemmas():
-                            synonyms.append(l.name())
-                    
-                    for y in synonyms:
-                        for z in range(len(listStringSecond)):
-                            if(y==listStringSecond[z]):
-                                listStringSecond[z]=x
-
-                inputStringSecond = ' '.join(map(str, listStringSecond))            
-                print(inputStringSecond)   
-                embeddings = model([self.sentences[int(level.val())-1]])
-                embeddingsRecvied = model([inputStringSecond])
+                embed_sentences = np.asarray(model.encode([self.sentences[int(level.val())-1]]))
+                embeddingsRecvied =np.asarray(model.encode([test.val()]))
                 method = methods[self.method]
                 if self.verbose:
                     logging.info(f"Calculating similarity between sentences...")
-                similarity = method(embeddingsRecvied, embeddings)
+                similarity = method(embeddingsRecvied, embed_sentences)
                 value = plot_similarity(self.sentences, similarity, self.method)
                 db.child("Test").child("Result").child("test").set("z")
                 db.child("Test").child("Result").child("result").set(str(value))
                 db.child("Test").child("Result").child("level").set("y")
             else:
                 continue
-        
 
-        #
