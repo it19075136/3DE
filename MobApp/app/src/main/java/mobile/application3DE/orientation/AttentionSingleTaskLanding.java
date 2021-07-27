8fc53e4c7ef90f92be87c59ac53b0b9f46d29a8a
Chirath <it18395242@my.sliit.lk>
2021-07-27 22:11:08 +0530

    Walking test UIs

--- MobApp/app/src/main/java/mobile/application3DE/orientation/AttentionSingleTaskLanding.java
+++ MobApp/app/src/main/java/mobile/application3DE/orientation/AttentionSingleTaskLanding.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 
+import com.google.android.material.textview.MaterialTextView;
+
 import mobile.application3DE.R;
 import mobile.application3DE.utilities.BaseActivity;
 
@@ -18,24 +20,33 @@ public class AttentionSingleTaskLanding extends BaseActivity {
 
     Intent testintent;
     String choiceIdentifier;
+    MaterialTextView type,instruct,note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attention_single_task_landing);
 
+        type = findViewById(R.id.singleTaskType);
+        instruct = findViewById(R.id.singleTaskInstruct);
+        note = findViewById(R.id.singleTaskNote);
+
         testintent = getIntent();
         choiceIdentifier = testintent.getStringExtra("choice");
+
+        if(choiceIdentifier.equals("speech")) {
+            testintent = new Intent(this, AttentionSpeechTest.class);
+        }
+        else {
+            type.setText("Walking Task");
+            instruct.setText("Keep walking until you hear a beep");
+            note.setVisibility(View.INVISIBLE);
+            testintent = new Intent(this, AttentionWalkingTest.class);
+        }
 
     }
 
     public void directToTest(View view) {
-
-        if(choiceIdentifier.equals("speech")){
-            testintent = new Intent(this, AttentionSpeechTest.class);
-            startActivity(testintent);
-        }
-
-        // else for walking activity
+        startActivity(testintent);
     }
 }
