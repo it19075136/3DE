c9e4074d64f2601749538f01d7f15519077f44d8
Avantha <avanthac@yahoo.com>
2021-09-19 16:06:23 +0530

    server get route added

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(express.json({limit: '50mb'}));
 
 app.use(express.urlencoded({limit: '50mb', extended: true}))
 
+app.get('/', (req,res) => {
+    res.send("Hi, I'm Up")
+});
+
 app.use('/speech/api', speechRoute);
 
 app.listen(PORT, () => {
