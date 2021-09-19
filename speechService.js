811a2dec0de7c3fce7a62bcffcd5eabece104f0b
Avantha <avanthac@yahoo.com>
2021-09-19 16:32:32 +0530

    server get route added

--- speechService.js
+++ speechService.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const speechAPI = require('./speechAPI');
 
 router.post('/', async (req,res) => {
+    console.log(req.body)
     const result = await speechAPI.main(req.body);
     res.json(result);
 });
