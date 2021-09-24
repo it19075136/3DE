ca5205c01fe60d14a8b8f4ab99e624a5fb5bed19
Avantha <avanthac@yahoo.com>
2021-09-24 17:58:39 +0530

    updated backend service

--- Attention_BE/server.js
+++ Attention_BE/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const speechRoute = require('./speechService');
+const speechRoute = require('./services/speechService');
+const mailRoute = require('./services/mailService');
 const PORT = process.env.PORT || 5000;
 
 let app = express();
@@ -11,7 +12,13 @@ app.use(express.json({limit: '50mb'}));
 
 app.use(express.urlencoded({limit: '50mb', extended: true}))
 
-app.use('/speech/api', speechRoute);
+app.get('/', (req,res) => {
+    res.send("Hi, I'm Up")
+});
+
+app.use('/speech/api', speechRoute); // speech api
+
+app.use('/mail/api', mailRoute); // mail api
 
 app.listen(PORT, () => {
     console.log("Listening on port: ", PORT);
